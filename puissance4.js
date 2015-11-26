91e5de76636a5f2589a802247acf62d05bd61709
Antoine <AntoineFr@users.noreply.github.com>
2015-11-26 13:43:02 +0100

    Changements d'indentation et de position de code
    Oui, on ne peut pas apporter des nouveautés à chaque fois :(

--- puissance4.js
+++ puissance4.js
@@ -1,12 +1,4 @@
-var jeu, i, j;
 var TAILLE_ROND = 30, ESPACEMENT = 3;
-jeu = [];
-for (i = 0; i < 7; i++) { // on crée une grille vide
-  jeu[i] = [];
-  for (j = 0; j < 5; j++) {
-    jeu[i][j] = '';
-  }
-}
 
 function AfficheGrille(jeu) {
   for (i = 0; i < 7; i++) {
@@ -25,51 +17,63 @@ function AfficheGrille(jeu) {
 function SaisieColonne(jeu, joueur) {
   var col, i;
   col = SaisieEntier("Dans quelle colonne voulez-vous jouer ?");
-  if(jeu[col][0] != ''){ return -1; } // la colonne est pleine
+  if (jeu[col][0] != '') {
+    return -1;
+  } // la colonne est pleine
   i = 0;
-  while(jeu[col][i] == '' && i < 4){
+  while (jeu[col][i] == '' && i < 4) {
     i++;
   }
   jeu[col][i] = joueur;
   return i;
 }
 
-function Termine(jeu){
+function Termine(jeu) {
   // Vérification des colonnes
-  for(i = 0 ; i < 2 ; i++){ // on fait le test pour les lignes 0 et 1 (il n'y a pas 4 cases en dessous après)
-    for(j = 0 ; j < 7 ; j++){ // chaque colonne
+  for (i = 0; i < 2; i++) { // on fait le test pour les lignes 0 et 1 (il n'y a pas 4 cases en dessous après)
+    for (j = 0; j < 7; j++) { // chaque colonne
       cas = jeu[i][j];
-      if(cas != '' && jeu[i+1][j] == cas && jeu[i+2][j] == cas && jeu[i+2][j] == cas){
+      if (cas != '' && jeu[i + 1][j] == cas && jeu[i + 2][j] == cas && jeu[i + 2][j] == cas) {
         return cas;
       }
     }
   }
   // Vérification des lignes
-  for(i = 0 ; i < 5 ; i++){ // chaque ligne
-    for(j = 0 ; j < 4 ; j++){ // on fait le test pour les cases 0 à 3
+  for (i = 0; i < 5; i++) { // chaque ligne
+    for (j = 0; j < 4; j++) { // on fait le test pour les cases 0 à 3
       cas = jeu[i][j];
-      if(cas != '' && jeu[i][j+1] == cas && jeu[i][j+2] == cas && jeu[i][j+3] == cas){
+      if (cas != '' && jeu[i][j + 1] == cas && jeu[i][j + 2] == cas && jeu[i][j + 3] == cas) {
         return cas;
       }
     }
   }
-  // Vérification de la diagonale haut gauche -> bas droite
-  for(i = 0 ; i < 2 ; i++){ // on fait le test pour les lignes 0 et 1
-    for(j = 0 ; j < 4 ; j++){ // on fait le test pour les cases 0 à 3
+  // Vérification des diagonales haut gauche -> bas droite
+  for (i = 0; i < 2; i++) { // on fait le test pour les lignes 0 et 1
+    for (j = 0; j < 4; j++) { // on fait le test pour les cases 0 à 3
       cas = jeu[i][j];
-      if(cas != '' && jeu[i+1][j+1] == cas && jeu[i+2][j+2] == cas && jeu[i+3][j+3] == cas){
+      if (cas != '' && jeu[i + 1][j + 1] == cas && jeu[i + 2][j + 2] == cas && jeu[i + 3][j + 3] == cas) {
         return cas;
       }
     }
   }
-  // Vérification de la diagonale bas gauche -> haut droite
-  for(i = 3 ; i < 5 ; i++){ // on fait le test pour les lignes 3 et 4
-    for(j = 0 ; j < 4 ; j++){ // on fait le test pour les cases 0 à 3
+  // Vérification des diagonales bas gauche -> haut droite
+  for (i = 3; i < 5; i++) { // on fait le test pour les lignes 3 et 4
+    for (j = 0; j < 4; j++) { // on fait le test pour les cases 0 à 3
       cas = jeu[i][j];
-      if(cas != '' && jeu[i-1][j+1] == cas && jeu[i-2][j+2] == cas && jeu[i-3][j+3] == cas){
+      if (cas != '' && jeu[i - 1][j + 1] == cas && jeu[i - 2][j + 2] == cas && jeu[i - 3][j + 3] == cas) {
         return cas;
       }
     }
   }
   return '';
+}
+
+// Création d'une grille vide
+var i, j;
+var jeu = [];
+for (i = 0; i < 7; i++) {
+  jeu[i] = [];
+  for (j = 0; j < 5; j++) {
+    jeu[i][j] = '';
+  }
 }
